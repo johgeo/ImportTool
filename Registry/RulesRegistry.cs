393921899c31e23fe8bc5f67e596382db896a645
John George <john.george@avensia.com>
2019-11-22 13:01:16 +0100

    Rename rule files for clarification

--- Registry/RulesRegistry.cs
+++ Registry/RulesRegistry.cs
@@ -14,14 +14,14 @@ namespace ProductImporterTool.Registry
                 scan.WithDefaultConventions();
             });
 
-            For<IValidationRule<M3ExcelDataModel>>().Add<Rules.SkuNumberMissing>();
-            For<IValidationRule<M3ExcelDataModel>>().Add<Rules.EanCodeMissing>();
-            For<IValidationRule<M3ExcelDataModel>>().Add<Rules.UnitOfMeasureMissing>();
-            For<IValidationRule<M3ExcelDataModel>>().Add<Rules.ColorCodeMissing>();
-            For<IValidationRule<M3ExcelDataModel>>().Add<Rules.ColorNameMissing>();
-            For<IValidationRule<M3ExcelDataModel>>().Add<Rules.ModelIsMissing>();
-            For<IValidationRule<M3ExcelDataModel>>().Add<Rules.ECommPlatformMissing>();
-            For<IValidationRule<M3ExcelDataModel>>().Add<Rules.StockPolicyWrongDataOrMissing>();
+            For<IValidationRule<M3ExcelDataModel>>().Add<M3Rules.SkuNumberMissing>();
+            For<IValidationRule<M3ExcelDataModel>>().Add<M3Rules.EanCodeMissing>();
+            For<IValidationRule<M3ExcelDataModel>>().Add<M3Rules.UnitOfMeasureMissing>();
+            For<IValidationRule<M3ExcelDataModel>>().Add<M3Rules.ColorCodeMissing>();
+            For<IValidationRule<M3ExcelDataModel>>().Add<M3Rules.ColorNameMissing>();
+            For<IValidationRule<M3ExcelDataModel>>().Add<M3Rules.ModelIsMissing>();
+            For<IValidationRule<M3ExcelDataModel>>().Add<M3Rules.ECommPlatformMissing>();
+            For<IValidationRule<M3ExcelDataModel>>().Add<M3Rules.StockPolicyWrongDataOrMissing>();
         }
     }
 }
---
+++ Validation/EnrichValidation/Rules/EnrichmentRules.cs
@@ -0,0 +1,29 @@
+﻿namespace ProductImporterTool.Validation.EnrichValidation.Rules
+{
+    public class EnrichmentRules
+    {
+        public class CategoryCodeFormatFaulty : ValidationRuleBase<EnrichmentExcelDataModel>
+        {
+            public override bool Validate(EnrichmentExcelDataModel model, out string message)
+            {
+                message = "Data in category field does not include an epi category code";
+                if (model.Categories.Contains(","))
+                {
+                    var codes = model.Categories.Split(',');
+                    foreach (var code in codes)
+                    {
+                        if (!code.Contains("_1"))
+                            return false;
+                    }
+                }
+                else
+                {
+                    if (!model.Categories.Contains("_1"))
+                        return false;
+                }
+
+                return true;
+            }
+        }
+    }
+}
